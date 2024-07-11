41738790fb76aeafb4ff5118ae6c588cbde25ec3
Nandan BU <167330500+bunandan@users.noreply.github.com>
2024-07-11 15:27:38 +0530

    Add files via upload

---
+++ 7 segment comman cathode.c
@@ -0,0 +1,21 @@
+#include<reg51.h>
+void delay(int k)
+{
+	int i,j;
+	for(i=0;i<k;i++)
+	for(j=0;j<1274;j++);
+}
+void main()
+{
+  unsigned char i;
+  unsigned char arr[20]={0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f,0x77,0x7c,0x39,0x5e,0x79,0x71};
+	P2=0x00;
+	while(1)
+	{
+		for(i=0;i<20;i++)
+		{
+			P2=arr[i];
+			delay(100);
+		}
+	}
+}
