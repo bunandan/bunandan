c91b8836b970a76dc20f536347f5313fc0bc1211
Nandan BU <167330500+bunandan@users.noreply.github.com>
2024-08-18 08:47:33 +0530

    Add files via upload

--- c++ program to check prime number.cpp
+++ c++ program to check prime number.cpp
@@ -24,26 +24,3 @@ int main()
     }
    return 0;
 }
-/*#include<iostream>
-
-using namespace std;
-
-int main() {
-  int num, i, isPrime=0;
-  cout << "Enter a Positive Integer\n";
-  cin >> num;
-  // Check whether num is divisible by any number between 2 to (num/2)
-  for(i = 2; i <=(num/2); ++i) {
-      if(num%i==0) {
-          isPrime=1;
-          break;
-      }
-  }
-
-  if(isPrime==0)
-      cout << num << " is a Prime Number";
-  else
-      cout << num << " is not a Prime Number";
-
-  return 0;
-}*/
