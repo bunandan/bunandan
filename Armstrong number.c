931eaf7d6be7dadf40b5e9738ec64b3cf5715f57
Nandan BU <167330500+bunandan@users.noreply.github.com>
2024-08-19 09:35:12 +0530

    Add files via upload

--- Armstrong number.c
+++ Armstrong number.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+/*#include<stdio.h>
 int main()
 {
     int n,arm=0,r;
@@ -22,5 +22,63 @@ int main()
         printf("%d is not a armstrong number\n",a);
     }
     return 0;
+}*/
+/*#include<stdio.h>
+int main()
+{
+
+    int n,arm=0,r;
+    printf("enter the number\n");
+    scanf("%d",&n);
+    int a=n;
+    while(n>0)
+    {
+        r=n%10;
+       arm=(r*r*r)+arm;
+       n=n/10;
+    }
+       if(a==arm)
+       {
+           printf("%d is a armstong number\n",a);
+
+       }
+       else
+       {
+           printf("%d is a not a armstrong number\n",a);
+       }
+
+       return 0;
+}*/
+
+/*#include<stdio.h>
+int main()
+{
+    int n,arm=0,r;
+    printf("enter the number\n");
+    scanf("%d",&n);
+    int a=n;
+    while(n>0)
+    {
+        r=n%10;
+        arm=(r*r*r)+arm;
+        n=n/10;
+    }
+    if(a==arm)
+    {
+        printf("%d is a armmstrong number\n",a);
+
+    }
+    else
+    {
+        printf("%d is not armstrong numben\n",a);
+    }
+    return 0;
+}*/
+#include<stdio.h>
+int main()
+{
+    int a,arm=0,r;
+
 }
+
 
