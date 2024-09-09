6e6c7992f1fc317e7dac0e359be2bc1aef501d19
Nandan BU <167330500+bunandan@users.noreply.github.com>
2024-09-09 21:22:18 +0530

    Add files via upload

--- C program to check whether a year is leap year or not in one line.c
+++ C program to check whether a year is leap year or not in one line.c
@@ -14,9 +14,7 @@ int main()
     }
     return 0;
 }*/
-
-
-#include<stdio.h>
+/*#include<stdio.h>
 int main()
 {
     int a;
@@ -33,4 +31,43 @@ int main()
             printf("%d is not aleap year\n",a);
         }
 }
+}*/
+/*#include<stdio.h>
+int main()
+{
+    int a;
+    for(int i=0;i<=4;i++)
+    {
+        printf("enter the year\n");
+        scanf("%d",&a);
+        if((a%4==0)&&(a%100!=0)||(a%400==0))
+        {
+            printf("%d is a leap year\n",a);
+
+        }
+        else
+        {
+            printf("%d is not a leap year\n",a);
+        }
+    }
+}*/
+#include<stdio.h>
+int main()
+{
+    int n,a;
+    printf("enter the number\n");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+        printf("enter the year\n");
+        scanf("%d",&a);
+        if((a%4==0)&&(a%100!=0)||(a%400==0))
+        {
+            printf("%d is a leap year\n",a);
+        }
+        else
+        {
+            printf("%d is not a leap year\n",a);
+        }
+    }
 }
