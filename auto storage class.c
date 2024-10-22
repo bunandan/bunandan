462232b0023f941beec9137e9cbbfef294101902
Nandan BU <167330500+bunandan@users.noreply.github.com>
2024-10-22 22:10:04 +0530

    Add files via upload

--- auto storage class.c
+++ auto storage class.c
@@ -12,7 +12,7 @@ int main()
     }
     printf("%d\n",i);
 }*/
-#include<stdio.h>
+/*#include<stdio.h>
 void fun()
 {
     register int x=1;
@@ -24,4 +24,15 @@ int main()
 fun();
 fun();
 return 0;
+}*/
+#include<stdio.h>
+int main()
+{
+    auto int i;
+    {
+        auto int i=10;
+        printf("%d\n",i);
+
+    }
+    printf("%d\n",i);
 }
